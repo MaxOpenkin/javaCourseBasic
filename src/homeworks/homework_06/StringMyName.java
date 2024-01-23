184351130117ad715a8fd9eec6457abf44bd018c
openk <openkin.max@gmail.com>
2024-01-24 04:17:36 +0600

    Homework_task_4_5 up

--- src/homeworks/homework_06/StringMyName.java
+++ src/homeworks/homework_06/StringMyName.java
@@ -2,10 +2,8 @@ package homeworks.homework_06;
 
 public class StringMyName {
 
-    String myName = "sgsgsdgs";
+    String myName = "";
 
     // Это задание уже было сделано в addHomework2 (task_2)
 
     }
-
-}
