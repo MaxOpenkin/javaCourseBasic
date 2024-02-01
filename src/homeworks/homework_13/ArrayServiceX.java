df12f2b08875c0b03567520faeef04aaa856364d
openk <openkin.max@gmail.com>
2024-02-02 02:42:16 +0600

    Homework_13 up

--- src/homeworks/homework_13/ArrayServiceX.java
+++ src/homeworks/homework_13/ArrayServiceX.java
@@ -4,21 +4,41 @@ import java.util.Arrays;
 
 public class ArrayServiceX {
 
-    public void replaceOddIndicesWithZero (int[] myArray1) {
-        for (int i = 1; i < myArray1.length; i = i + 2) {
-            myArray1[i] = 0;
+    public void replaceOddIndicesWithZero (int[] array) {
+        for (int i = 1; i < array.length; i = i + 2) {
+            array[i] = 0;
         }
-        System.out.println("Массив после замены элементов с нечетным индексом на ноль: " + Arrays.toString(myArray1));
+        System.out.println("Массив после замены элементов с нечетным индексом на ноль: " + Arrays.toString(array));
     }
 
-    public void checkStrictlyIncreasing (int[] myArray2){
-        for (int i = 1; i < myArray2.length; i++) {
-            if (myArray2[i] <=  myArray2[i - 1]){
+    public void checkStrictlyIncreasing (int[] array1){
+        for (int i = 1; i < array1.length; i++) {
+            if (array1[i] <=  array1[i - 1]){
                 System.out.println("Массив не является строго возрастающей последовательностью.");
                 return;
             }
         }
         System.out.println("Массив является строго возрастающей последовательностью.");
+    }
+
+    public int[] createArrayTask1 () {
+        int[] array = new int[8];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 50) + 1;
+        }
+        System.out.println("Массив: " + Arrays.toString(array));
+        return array;
+    }
+
+    public int[] createArrayTask2 () {
+        int[] array1 = new int[5];
+
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = (int) (Math.random() * 99) + 10;
+        }
+        System.out.println("Массив: " + Arrays.toString(array1));;
+        return array1;
     }
 
 }
--- src/homeworks/homework_13/ReplaceArrayDemo.java
+++ src/homeworks/homework_13/ReplaceArrayDemo.java
@@ -1,24 +1,30 @@
 package homeworks.homework_13;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class ReplaceArrayDemo {
 
     public static void main(String[] args) {
 
-        int[] myArray1 = {8,2,48,35,7,13,16,9};
+        //int[] myArray1 = {8,2,48,35,7,13,16,9};
+        //int[] myArray1 = new Random().ints(8, 1, 51).toArray();
 
-        System.out.println("Наш первый массив: " + Arrays.toString(myArray1));
 
         ArrayServiceX service = new ArrayServiceX();
 
-        service.replaceOddIndicesWithZero(myArray1);
+        int[] array = service.createArrayTask1();
 
-        int[] myArray2 = {10,86,17,99,42};
+        service.replaceOddIndicesWithZero(array);
 
-        System.out.println("Наш второй массив: " + Arrays.toString(myArray2));
 
-        service.checkStrictlyIncreasing(myArray2);
+        //int[] myArray2 = {10,86,17,99,42};
+        //int[] myArray2 = new Random().ints(5, 10, 99).toArray();
+
+
+        int[] array1 = service.createArrayTask2();
+
+        service.checkStrictlyIncreasing(array1);
 
     }
 }
